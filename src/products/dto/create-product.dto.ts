64fe1ec98a908696c599f6ba0b20c69f8335ee4d
emanuelhardwell <emanuelnoe16@gmail.com>
2024-11-24 23:47:47 -0600

    feat: Create ProductDto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,37 @@
-export class CreateProductDto {}
+import {
+  IsIn,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(2)
+  title: string;
+
+  @IsNumber()
+  @IsPositive()
+  price: number;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @MinLength(2)
+  slug: string;
+
+  @IsInt()
+  @IsPositive()
+  stock: number;
+
+  @IsString({ each: true })
+  sizes: string[];
+
+  @IsIn(['men', 'women', 'kid', 'unisex'])
+  gender: string;
+}
