90803eeaf2828c7c4c0d9b6f3281a88b97ef2325
emanuelhardwell <emanuelnoe16@gmail.com>
2025-01-18 22:07:51 -0600

    feat: Config the seedModule

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -10,5 +10,6 @@ import { ProductImage } from './entities/product-image.entity';
   imports: [TypeOrmModule.forFeature([Product, ProductImage]), CommonModule],
   controllers: [ProductsController],
   providers: [ProductsService],
+  exports: [ProductsService, TypeOrmModule],
 })
 export class ProductsModule {}
