996a3f2bda385799bc2f117059f134036e6f959a
emanuelhardwell <emanuelnoe16@gmail.com>
2025-01-20 21:42:06 -0600

    feat: Create controller and service findFile

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { HandleError } from '../common/exceptions/handle-error';
+import { join } from 'path';
+import { existsSync } from 'fs';
 
 @Injectable()
 export class FilesService {
@@ -8,6 +10,20 @@ export class FilesService {
   uploadFile() {
     try {
       return 'File uploaded successfully';
+    } catch (error) {
+      this.handleError.handleErrorService(error);
+    }
+  }
+
+  findFile(id: string) {
+    try {
+      const path = join(__dirname, '../../static/products/', id);
+
+      if (!existsSync(path)) {
+        throw new BadRequestException(`File not found with id ${id}!`);
+      }
+
+      return path;
     } catch (error) {
       this.handleError.handleErrorService(error);
     }
