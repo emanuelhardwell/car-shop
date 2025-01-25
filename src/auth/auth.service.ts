541fd06c9eda726dde79418f3a605c1daac1eb44
emanuelhardwell <emanuelnoe16@gmail.com>
2025-01-24 22:09:43 -0600

    feat: Create controller and service loginUser

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { HandleError } from '../common/exceptions/handle-error';
 import { Repository } from 'typeorm';
@@ -7,6 +7,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Injectable()
 export class AuthService {
@@ -41,6 +42,40 @@ export class AuthService {
   private getJwtToken(payload: JwtPayload): string {
     try {
       return this.jwtService.sign(payload);
+    } catch (error) {
+      this.handleError.handleErrorService(error);
+    }
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto;
+    try {
+      const user = await this.userRepository.findOne({
+        where: { email },
+        select: {
+          id: true,
+          email: true,
+          password: true,
+          isActice: true,
+          fullName: true,
+        },
+      });
+      console.log(user);
+
+      if (!user) throw new UnauthorizedException('Credentials not valid');
+      if (!user.isActice) throw new UnauthorizedException('User is inactive');
+      if (!bcrypt.compareSync(password, user.password))
+        throw new UnauthorizedException('User or Password not valid');
+
+      delete user.password;
+      return {
+        ...user,
+        token: this.getJwtToken({
+          id: user.id,
+          email: user.email,
+          fullName: user.fullName,
+        }),
+      };
     } catch (error) {
       this.handleError.handleErrorService(error);
     }
