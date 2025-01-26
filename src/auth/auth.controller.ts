e9beb3b320fb6d2abdd9541fe2dcab6781801ed3
emanuelhardwell <emanuelnoe16@gmail.com>
2025-01-26 10:42:00 -0600

    feat: Create UserRoleGuard and apply

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  UseGuards,
+  SetMetadata,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
@@ -6,6 +13,7 @@ import { AuthGuard } from '@nestjs/passport';
 import { GetUserDecorator } from './decorators/get-user.decorator';
 import { User } from './entities/user.entity';
 import { RawHeadersDecorator } from './decorators/raw-headers.decorator';
+import { UserRoleGuard } from './guards/user-role/user-role.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -23,6 +31,7 @@ export class AuthController {
   @Get('private')
   @UseGuards(AuthGuard())
   privateDefault(
+    // @Req() request: Express.Request, ## NO se debe sacar la INFO directamente del REQUEST, (se debe crear un Decorador)
     @GetUserDecorator() user: User,
     @GetUserDecorator('email') email: string,
     @RawHeadersDecorator() rawHeaders: object,
@@ -30,5 +39,13 @@ export class AuthController {
   ) {
     const res = this.authService.privateDefault();
     return { ...res, user, email, rawHeaders, header };
+  }
+
+  @Get('private2')
+  @SetMetadata('roles', ['admin', 'super-admin']) //SetMetadata: ya NO se recomienda usar porque no podemos equivocar facilmente en el MetadataKey
+  @UseGuards(AuthGuard(), UserRoleGuard)
+  privateWithCustomGuard(@GetUserDecorator() user: User) {
+    const res = this.authService.privateDefault();
+    return { ...res, user };
   }
 }
